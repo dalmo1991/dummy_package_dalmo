d0356bc4e7e60a8d7ccbaf61230250a99f930a56
Marco Dal Molin <marco.dalmolin@eawag.ch>
2020-09-11 16:45:47 +0200

    Added dummy test

---
+++ tests/unittest/test_sum.py
@@ -0,0 +1,15 @@
+import unittest
+import sys
+from os.path import abspath, join, dirname
+
+import_folder = join(abspath(dirname(__file__)), '..', '..', 'dummy_package_dalmo')
+sys.path.insert(0, import_folder)
+
+from functions import sum
+
+class TestSum(unittest.TestCase):
+
+    def test_result(self):
+        self.assertEqual(sum(2, 2), 4)
+
+unittest.main()
